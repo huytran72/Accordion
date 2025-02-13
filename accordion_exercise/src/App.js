6c394fc8fcbb28c7a77316b89aa1889b313bc05b
Huy Tran <baohuy82.th@gmail.com>
2025-02-13 00:02:30 -0800

    Update App.js

--- accordion_exercise/src/App.js
+++ accordion_exercise/src/App.js
@@ -39,7 +39,7 @@ function AccordionItem({ num, title, text }) {
   return (
     <div className="item">
       <p className="number">{num}</p>
-      <p className="text">{text}</p>
+      <p className="text">{title}</p>
       <p className="icon">-</p>
       <div className="content-box">title</div>
     </div>
