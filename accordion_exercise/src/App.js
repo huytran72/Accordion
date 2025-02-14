6e399dfaab021fcdb41e418ff8d865f8ead3705a
Huy Tran <baohuy82.th@gmail.com>
2025-02-13 23:56:39 -0800

    Update App.js

--- accordion_exercise/src/App.js
+++ accordion_exercise/src/App.js
@@ -44,7 +44,7 @@ function AccordionItem({ num, title, text }) {
   }
 
   return (
-    <div className="item" onClick={handleToggle}>
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="text">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
