7a8a895652bc96f9b1e0045c92684978adcb4259
Huy Tran <baohuy82.th@gmail.com>
2025-02-13 23:48:18 -0800

    Update App.js

--- accordion_exercise/src/App.js
+++ accordion_exercise/src/App.js
@@ -38,7 +38,7 @@ function Accordion({ data }) {
 function AccordionItem({ num, title, text }) {
   return (
     <div className="item">
-      <p className="number">{num}</p>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="text">{title}</p>
       <p className="icon">-</p>
       <div className="content-box">{text}</div>
