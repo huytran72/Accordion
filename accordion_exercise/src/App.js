16d2e42188881661cbcc735ea6bd90452fa8e984
Huy Tran <baohuy82.th@gmail.com>
2025-02-13 23:50:37 -0800

    Update App.js

--- accordion_exercise/src/App.js
+++ accordion_exercise/src/App.js
@@ -44,7 +44,8 @@ function AccordionItem({ num, title, text }) {
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="text">{title}</p>
       <p className="icon">-</p>
-      <div className="content-box">{text}</div>
+
+      {isOpen && <div className="content-box">{text}</div>}
     </div>
   );
 }
